f33e5829a4cfbcd5d9770311e645a7cb5f683421
issa-abdulaziz <issaabdulaziz20919@gmail.com>
2022-08-07 18:06:10 +0300

    update employee pages

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -1,127 +1,110 @@
 @extends('layouts.app')
 @section('content')
-  <div class="d-flex justify-content-between align-items-center mb-3">
-    <h3>Employees</h3>
-    <a href="/employee/create" class="btn btn-primary">Add New One</a>
-  </div>
-  @if (count($employees) > 0)
-    <table class="table display nowrap" id="table_id" style="width:100%">
-      <thead class="thead-light">
-        <tr>
-          <th data-priority="1">#</th>
-          <th data-priority="3">Full Name</th>
-          <th data-priority="7">Phone Number</th>
-          <th data-priority="4">Postion</th>
-          <th data-priority="10">Vacation Days</th>
-          <th data-priority="9">Vacation Start Count At</th>
-          <th data-priority="11">City</th>
-          <th data-priority="8">Hired At</th>
-          <th data-priority="5">Salary</th>
-          <th data-priority="6">Active</th>
-          <th data-priority="2"></th>
-          <th data-priority="2"></th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach ($employees as $employee)
-          <tr>
-            <td>{{ $loop->index + 1 }}</td>
-            <td><a href="{{ route('employee.show', $employee->id) }}">{{ $employee->full_name }}</a></td>
-            <td>{{ $employee->phone_number }}</td>
-            <td>{{ $employee->position }}</td>
-            <td>{{ $employee->vacation_days }}</td>
-            <td>{{ $employee->vacation_start_count_at }}</td>
-            <td>{{ $employee->city }}</td>
-            <td>{{ $employee->hired_at }}</td>
-            <td>{{ $employee->salary }} {{ session('setting')->currency }}</td>
-            <td>
-              @if ($employee->active)
-                <i class="fa fa-check-square" style="font-size: 18px;color:rgb(0,128,255)"></i>
-              @else
-                <i class="fa fa-square" style="font-size: 18px;color:rgb(0,128,255)"></i>
-              @endif
-            </td>
-            <td><a class="btn btn-success btn-sm" href="{{ route('employee.edit', $employee->id) }}"><i
-                  class="fas fa-edit"></i></a></td>
-            <td>
-              <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#confirmDeletionModal"
-                data-id="{{ $employee->id }}" data-name="{{ $employee->full_name }}"
-                data-date="{{ $employee->hired_at }}">
-                <i class="fas fa-trash"></i>
-              </button>
-            </td>
-          </tr>
-        @endforeach
-      </tbody>
-    </table>
-    @include('inc.confirmDeletion', [
-        'title' => 'employee',
-    ])
-  @else
-    <p>No Employee Added Yet</p>
-  @endif
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Employees</h1>
+                </div>
+                <div class="col-sm-6 text-right">
+                    <a href="{{ route('employee.create') }}" class="btn btn-success">Add New Employee</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            @if (count($employees) > 0)
+                                <table class="datatable table-striped table-hover nowrap table" width="100%">
+                                    <thead class="thead-dark">
+                                        <tr>
+                                            <th data-hide-footer-filter="true">#</th>
+                                            <th>Full Name</th>
+                                            <th>Phone Number</th>
+                                            <th>Postion</th>
+                                            <th>Salary</th>
+                                            <th>Hired At</th>
+                                            <th>City</th>
+                                            <th>Vacation Days</th>
+                                            <th>Vacation Start Count At</th>
+                                            <th>Active</th>
+                                            <th data-hide-footer-filter="true" class="text-right">Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($employees as $employee)
+                                            <tr>
+                                                <td>{{ $loop->iteration }}</td>
+                                                <td>
+                                                    <a href="{{ route('employee.show', $employee->id) }}">{{ $employee->full_name }}</a>
+                                                </td>
+                                                <td>{{ $employee->phone_number }}</td>
+                                                <td>{{ $employee->position }}</td>
+                                                <td>{{ $employee->salary }} {{ session('setting')->currency }}</td>
+                                                <td>{{ $employee->hired_at }}</td>
+                                                <td>{{ $employee->city }}</td>
+                                                <td>{{ $employee->vacation_days }}</td>
+                                                <td>{{ $employee->vacation_start_count_at }}</td>
+                                                <td>
+                                                    @if ($employee->active)
+                                                        <span class="badge badge-pill badge-success">active</span>
+                                                    @else
+                                                        <span class="badge badge-pill badge-danger">in-active</span>
+                                                    @endif
+                                                </td>
+                                                <td>
+                                                    <a class="btn btn-primary btn-sm"
+                                                        href="{{ route('employee.edit', $employee->id) }}">
+                                                        <i class="fas fa-edit"></i>
+                                                    </a>
+                                                    <button type="button" class="btn btn-danger btn-sm" data-toggle="modal"
+                                                        data-target="#confirmDeletionModal" data-id="{{ $employee->id }}"
+                                                        data-name="{{ $employee->full_name }}"
+                                                        data-date="{{ $employee->hired_at }}">
+                                                        <i class="fas fa-trash"></i>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                                @include('inc.confirmDeletion', [
+                                    'title' => 'employee',
+                                ])
+                            @else
+                                <p>No Employee Added Yet</p>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- /.content -->
 @endsection
 @push('script')
-  <script>
-    $(document).ready(function() {
-      $('#table_id').DataTable({
-        dom: 'Bfrtip',
-        buttons: [
-          'pageLength',
-          'copyHtml5',
-          'excel',
-          'csvHtml5',
-          'pdfHtml5',
-          'print',
-        ],
-        lengthMenu: [
-          [10, 25, 50, -1],
-          ['10 rows', '25 rows', '50 rows', 'Show all']
-        ],
-        responsive: true,
-        scrollY: "400px",
-        "scrollCollapse": true,
-        "columnDefs": [{
-          "orderable": false,
-          "searchable": false,
-          "targets": [-1, -2]
-        }, ],
-        "pagingType": "full_numbers",
-        language: {
-          paginate: {
-            next: '>',
-            previous: '<',
-            first: '<<',
-            last: '>>'
-          }
-        },
-        stateSave: true,
-      });
-    });
-    /*
-    buttons: [
-            {
-                extend:    'copyHtml5',
-                text:      '<i class="fa fa-files-o"></i>',
-                titleAttr: 'Copy'
-            },
-            {
-                extend:    'excelHtml5',
-                text:      '<i class="fa fa-file-excel-o"></i>',
-                titleAttr: 'Excel'
-            },
-            {
-                extend:    'csvHtml5',
-                text:      '<i class="fa fa-file-text-o"></i>',
-                titleAttr: 'CSV'
-            },
-            {
-                extend:    'pdfHtml5',
-                text:      '<i class="fa fa-file-pdf-o"></i>',
-                titleAttr: 'PDF'
-            }
-        ]
-        */
-  </script>
-  <script type="text/javascript" src="{{ asset('js/_custom/confirmDeletion.js') }}" defer></script>
+    <script type="text/javascript" src="{{ asset('js/_custom/confirmDeletion.js') }}" defer></script>
 @endpush
