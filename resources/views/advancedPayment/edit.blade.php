208d3c6832560c794e085cf946d82d8c6b92cab1
issa-abdulaziz <issaabdulaziz20919@gmail.com>
2022-08-09 21:57:28 +0300

    update advanced payment pages layout

--- resources/views/advancedPayment/edit.blade.php
+++ resources/views/advancedPayment/edit.blade.php
@@ -1,58 +1,83 @@
 @extends('layouts.app')
 @section('content')
-  <div class="d-flex justify-content-between align-items-center mb-3">
-    <h3>Edit Advanced Payment</h3>
-    <a href="{{ url()->previous() }}" class="btn btn-primary">Back</a>
-  </div>
-  <form action="{{ route('advanced-payment.update', $advancedPayment->id) }}" method="POST">
-    @csrf
-    @method('PUT')
-    <div class="form-group">
-      <label for="employee_id">Employee</label>
-      <select id="employee_id" class="custom-select" name="employee_id">
-        <option disabled selected>select employee</option>
-        @foreach ($employees as $employee)
-          <option value="{{ $employee->id }}" {{ $advancedPayment->employee_id == $employee->id ? 'selected' : '' }}>
-            {{ $employee->full_name }}</option>
-        @endforeach
-      </select>
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Edit Advanced Payment</h1>
+                </div>
+                <div class="col-sm-6 text-right">
+                    <a href="{{ url()->previous() }}" class="btn btn-primary">Back</a>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="form-group">
-      <label for="date">Date</label>
-      <input type="date" value="{{ $advancedPayment->date }}" id="date" name="date"
-        min="{{ $advancedPayment->employee->hired_at }}" class="form-control" required>
-    </div>
-    <div class="form-group">
-      <label for="amount">Amount (in {{ session('setting')->currency }})</label>
-      <input type="number" value="{{ $advancedPayment->amount }}" id="amount" name="amount" class="form-control"
-        min="1" required>
-    </div>
-    <div class="form-group">
-      <label for="note">Note</label>
-      <textarea class="form-control" name="note" id="note" cols="30" rows="5">{{ $advancedPayment->note }}</textarea>
-    </div>
-    <button class="btn btn-primary" name="submit" id="submit" type="submit">Submit</button>
-  </form>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <form action="{{ route('advanced-payment.update', $advancedPayment->id) }}" method="POST">
+                                @csrf
+                                @method('PUT')
+                                <div class="form-group">
+                                    <label for="employee_id">Employee</label>
+                                    <select id="employee_id" class="custom-select" name="employee_id">
+                                        <option disabled selected>select employee</option>
+                                        @foreach ($employees as $employee)
+                                            <option value="{{ $employee->id }}"
+                                                {{ $advancedPayment->employee_id == $employee->id ? 'selected' : '' }}>
+                                                {{ $employee->full_name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="date">Date</label>
+                                    <input type="date" value="{{ $advancedPayment->date }}" id="date" name="date"
+                                        min="{{ $advancedPayment->employee->hired_at }}" class="form-control" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="amount">Amount (in {{ session('setting')->currency }})</label>
+                                    <input type="number" value="{{ $advancedPayment->amount }}" id="amount"
+                                        name="amount" class="form-control" min="1" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="note">Note</label>
+                                    <textarea class="form-control" name="note" id="note" cols="30" rows="5">{{ $advancedPayment->note }}</textarea>
+                                </div>
+                                <button class="btn btn-success btn-block w-50 mx-auto" name="submit" id="submit" type="submit">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
 @push('script')
-  <script>
-    $('body').on('change', '#employee_id', function() {
-      $.ajaxSetup({
-        headers: {
-          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-      });
-      $.ajax({
-        type: "get",
-        url: "{{ route('advanced-payment.getData', ['%id%']) }}".replace('%id%', $('#employee_id').val()),
-        dataType: 'json',
-        success: function(response) {
-          $('#date').attr('min', response.hired_at);
-        },
-        error: function(data) {
-          console.log('Error:', data);
-        }
-      });
-    });
-  </script>
+    <script>
+        $('body').on('change', '#employee_id', function() {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            $.ajax({
+                type: "get",
+                url: "{{ route('advanced-payment.getData', ['%id%']) }}".replace('%id%', $('#employee_id')
+                    .val()),
+                dataType: 'json',
+                success: function(response) {
+                    $('#date').attr('min', response.hired_at);
+                },
+                error: function(data) {
+                    console.log('Error:', data);
+                }
+            });
+        });
+    </script>
 @endpush
--- resources/views/advancedPayment/index.blade.php
+++ resources/views/advancedPayment/index.blade.php
@@ -1,100 +1,139 @@
 @extends('layouts.app')
 @section('content')
-  <div class="d-flex justify-content-between align-items-center mb-3">
-    <h3>Advanced Payment</h3>
-    <a href="/advanced-payment/create" class="btn btn-primary">Add New One</a>
-  </div>
-  <form action="{{ route('advanced-payment.index') }}" method="GET" id="form-filter">
-    @csrf
-    <input type="month" id="date" name="date" value="{{ $date }}" class="form-control my-3">
-  </form>
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Advanced Payment</h1>
+                </div>
+                <div class="col-sm-6 text-right">
+                    <a href="{{ route('advanced-payment.create') }}" class="btn btn-success">Add New One</a>
+                </div>
+            </div>
+        </div>
+    </div>
 
-  @if (count($advancedPayments) > 0)
-    <table class="table display nowrap" id="table_id" style="width:100%">
-      <thead class="thead-light">
-        <tr>
-          <th data-priority="1">#</th>
-          <th data-priority="3">Full Name</th>
-          <th data-priority="4">Date</th>
-          <th data-priority="5">Amount</th>
-          <th data-priority="6">Note</th>
-          <th data-priority="2"></th>
-          <th data-priority="2"></th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach ($advancedPayments as $advancedPayment)
-          <tr>
-            <td>{{ $loop->index + 1 }}</td>
-            <td><a
-                href="{{ route('employee.show', $advancedPayment->employee->id) }}">{{ $advancedPayment->employee->full_name }}</a>
-            </td>
-            <td>{{ $advancedPayment->date }}</td>
-            <td>{{ $advancedPayment->amount }} {{ session('setting')->currency }}</td>
-            <td title="{{ $advancedPayment->note }}">{{ $advancedPayment->note }}</td>
-            <td><a class="btn btn-success btn-sm" href="{{ route('advanced-payment.edit', $advancedPayment->id) }}"><i
-                  class="fas fa-edit"></i></a></td>
-            <td>
-              <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#confirmDeletionModal"
-                data-id="{{ $advancedPayment->id }}" data-name="{{ $advancedPayment->employee->full_name }}"
-                data-date="{{ $advancedPayment->date }}" data-amount=" {{ $advancedPayment->amount }}"
-                data-currency="{{ session('setting')->currency }}">
-                <i class="fas fa-trash"></i>
-              </button>
-            </td>
-          </tr>
-        @endforeach
-      </tbody>
-    </table>
-    @include('inc.confirmDeletion', [
-        'title' => 'advanced-payment',
-    ])
-  @else
-    <p>No Advanced Payment Added Yet</p>
-  @endif
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <form action="{{ route('advanced-payment.index') }}" method="GET" id="form-filter">
+                                @csrf
+                                <input type="month" id="date" name="date" value="{{ $date }}"
+                                    class="form-control my-3">
+                            </form>
+
+                            @if (count($advancedPayments) > 0)
+                                <table class="datatable table-striped table-hover nowrap table" width="100%">
+                                    <thead class="thead-dark">
+                                        <tr>
+                                            <th style="width: 4%;" data-hide-footer-filter="true">#</th>
+                                            <th style="width: 26%;">Full Name</th>
+                                            <th style="width: 15%;">Date</th>
+                                            <th style="width: 15%;">Amount</th>
+                                            <th style="width: 30%;">Note</th>
+                                            <th style="width: 10%;" data-hide-footer-filter="true" class="text-right">Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($advancedPayments as $advancedPayment)
+                                            <tr>
+                                                <td>{{ $loop->index + 1 }}</td>
+                                                <td>
+                                                    <a href="{{ route('employee.show', $advancedPayment->employee->id) }}">{{ $advancedPayment->employee->full_name }}</a>
+                                                </td>
+                                                <td>{{ $advancedPayment->date }}</td>
+                                                <td>{{ $advancedPayment->amount }} {{ session('setting')->currency }}</td>
+                                                <td title="{{ $advancedPayment->note }}">{{ $advancedPayment->note }}</td>
+                                                <td class="text-right">
+                                                    <a class="btn btn-success btn-sm"
+                                                        href="{{ route('advanced-payment.edit', $advancedPayment->id) }}">
+                                                        <i class="fas fa-edit"></i>
+                                                    </a>
+                                                    <button type="button" class="btn btn-danger btn-sm" data-toggle="modal"
+                                                        data-target="#confirmDeletionModal"
+                                                        data-id="{{ $advancedPayment->id }}"
+                                                        data-name="{{ $advancedPayment->employee->full_name }}"
+                                                        data-date="{{ $advancedPayment->date }}"
+                                                        data-amount=" {{ $advancedPayment->amount }}"
+                                                        data-currency="{{ session('setting')->currency }}">
+                                                        <i class="fas fa-trash"></i>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                                @include('inc.confirmDeletion', [
+                                    'title' => 'advanced-payment',
+                                ])
+                            @else
+                                <p>No Advanced Payment Added Yet</p>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
 @push('script')
-  <script>
-    $(document).ready(function() {
-      $('#table_id').DataTable({
-        dom: 'Bfrtip',
-        buttons: [
-          'pageLength',
-          'copyHtml5',
-          'excel',
-          'csvHtml5',
-          'pdfHtml5',
-          'print',
-        ],
-        lengthMenu: [
-          [10, 25, 50, -1],
-          ['10 rows', '25 rows', '50 rows', 'Show all']
-        ],
-        responsive: true,
-        scrollY: "400px",
-        "scrollCollapse": true,
-        "columnDefs": [{
-          "orderable": false,
-          "searchable": false,
-          "targets": [-1, -2]
-        }, ],
-        "pagingType": "full_numbers",
-        language: {
-          paginate: {
-            next: '>',
-            previous: '<',
-            first: '<<',
-            last: '>>'
-          }
-        },
-        stateSave: true,
-      });
+    <script>
+        $(document).ready(function() {
+            $('#table_id').DataTable({
+                dom: 'Bfrtip',
+                buttons: [
+                    'pageLength',
+                    'copyHtml5',
+                    'excel',
+                    'csvHtml5',
+                    'pdfHtml5',
+                    'print',
+                ],
+                lengthMenu: [
+                    [10, 25, 50, -1],
+                    ['10 rows', '25 rows', '50 rows', 'Show all']
+                ],
+                responsive: true,
+                scrollY: "400px",
+                "scrollCollapse": true,
+                "columnDefs": [{
+                    "orderable": false,
+                    "searchable": false,
+                    "targets": [-1, -2]
+                }, ],
+                "pagingType": "full_numbers",
+                language: {
+                    paginate: {
+                        next: '>',
+                        previous: '<',
+                        first: '<<',
+                        last: '>>'
+                    }
+                },
+                stateSave: true,
+            });
 
-      $('#date').change(function() {
-        $('#form-filter').submit();
-      });
+            $('#date').change(function() {
+                $('#form-filter').submit();
+            });
 
-    });
-  </script>
-  <script type="text/javascript" src="{{ asset('js/_custom/confirmDeletion.js') }}"></script>
+        });
+    </script>
+    <script type="text/javascript" src="{{ asset('js/_custom/confirmDeletion.js') }}"></script>
 @endpush
